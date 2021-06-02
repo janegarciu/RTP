94d4e4adddd6b32f1c80885e87539657bd6a7598
Eugenia <eugenia.garciu@isa.utm.md>
2021-06-02 09:13:34 +0300

    moved server and client to different projects to run docker compose

---
+++ Lab1/Server/src/main/scala/server/Main.scala
@@ -0,0 +1,20 @@
+package server
+
+import java.net.InetSocketAddress
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.util.ByteString
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val host = "localhost"
+    val port = 9900
+    println(s"Server started! listening to ${host}:${port}")
+    val actorSystem: ActorSystem = ActorSystem.create("myServerActorSystem")
+    val handler = actorSystem.actorOf(Props[SimplisticHandler])
+    val serverProps = TcpServer.props(new InetSocketAddress(host, port), handler)
+
+    val serverActor: ActorRef = actorSystem.actorOf(serverProps)
+    serverActor ! ByteString("Starting server...")
+  }
+}
---
+++ Lab1/Server/src/main/scala/server/TcpServer.scala
@@ -0,0 +1,37 @@
+package server
+
+import java.net.InetSocketAddress
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.io.{IO, Tcp}
+import akka.util.ByteString
+
+object TcpServer {
+  def props(remote: InetSocketAddress, handler: ActorRef) =
+    Props(new TcpServer(remote,handler))
+}
+
+class TcpServer(remote: InetSocketAddress,handler: ActorRef) extends Actor {
+
+  import akka.io.Tcp._
+  import context.system
+
+  IO(Tcp) ! Bind(self, remote)
+
+  def receive = {
+    case b @ Bound(localAddress) =>
+      context.parent ! b
+
+    case CommandFailed(_: Bind) ⇒ context stop self
+
+    case c @ Connected(remote, local) =>
+      println(s"Client connected - Remote(Client): ${remote.getAddress} Local(Server): ${local.getAddress}")
+
+      val connection = sender()
+      connection ! Register(handler)
+    case data: ByteString =>
+//      println("Sending request data to handler: " + data.utf8String)
+
+  }
+
+}
